aced23d158d1ef2ca274261d8592087dd23e0138
Aqudi <gjdigj@naver.com>
2020-06-17 11:32:26 +0900

    Apply degree base sort

--- BD_FinalProject/src/main/scala/bigdata/Task3.scala
+++ BD_FinalProject/src/main/scala/bigdata/Task3.scala
@@ -7,66 +7,91 @@ object Task3 {
 
   def main(args: Array[String]): Unit = {
     val input = args(0)
-    val degrees = args(1)
+    val input2 = args(1)
     val output = args(2)
 
     //  Spark settings
     val conf = new SparkConf().setAppName("Final project app").setMaster("local")
     val sc = new SparkContext(conf)
-//    sc.hadoopConfiguration.set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
+    //    sc.hadoopConfiguration.set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
 
-    /* Degree 정보를 사용해서 정렬할 수 있도록 세팅 */
-    //    val degree_db = sc.textFile(degrees + "/*")
-    //      .map(_.split("\t"))
-    //      .map(a => (a(0), a(1)))
-    //      .toDF("vertex", "degree")
-    //      .cache()
 
-    println(s"\nInput: $input")
-    println(s"Input2: $degrees")
+    println(s"========== INFO ==========")
+    println(s"Input: $input")
+    println(s"Input2: $input2")
     println(s"Output: $output\n")
 
+    // 만약 이전 결과가 남아있다면 제거
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    val outputPath = new Path(output)
+    if (fs.exists(outputPath)) {
+      fs.delete(outputPath, true)
+    }
 
-    /* Step 1 : Edge 불러와서 Wedge 찾기 */
+    /* Step 1 : Edge 불러와서 Wedge 만들기 */
     println("Step 1: Start")
-    val inputRDD = sc.textFile(input + "/*")
-    val edges = inputRDD
+
+    val edges = sc.textFile(input + "/part-r-*")
+      .map(_.split("\t"))
+      .map(x => (x(0).toInt, x(1).toInt))
+    val degress = sc.textFile(input2 + "/part-r-*")
+      .map(_.split("\t"))
+      .map(x => (x(0).toInt, x(1).toInt))
+
+    val edgeWithDegree = degress
+      .join(edges, 2)
+      .map { case (u, (ud, v)) => (v, (u, ud)) }
+      .join(degress, 2)
+      .map { case (v, ((u, ud), vd)) => ((u, ud), (v, vd)) }
       .map {
-        line =>
-          // Edge 를 불러와 Split 한 후 edge 를 만들어 return 해준다.
-          val edge = line.split("\t")
-          val u = edge(0).toInt
-          val v = edge(1).toInt
-          (u, v)
+        // 중간 산출물의 크기를 줄이기 위해서 Degree 가 더 작은
+        // 노드를 Key 로 사용하도록 한다.
+        case ((u, ud), (v, vd)) =>
+          if (ud < vd) {
+            ((u, ud), (v, vd))
+          } else {
+            ((v, vd), (u, ud))
+          }
       }
 
-    // 하둡에서 처럼 방출된 Edge 들을 Key vertex 기준으로 묶은 뒤
-    val wedges = edges
+
+    // 하둡에서 처럼 방출된 Edge 들을 Key 노드를 기준으로 묶어준 준뒤
+    // Key 노드 중심으로 모인 노드들 중 중복되지 않게 2개를 뽑아 Wedge 로 만들어준다.
+    val wedges = edgeWithDegree
       .groupByKey()
       .map {
-        edge =>
-          val u = edge._1.toInt
-          val values = edge._2.map(_.toInt).toList
+        case ((u, ud), vs) =>
+          val values = vs.toList
+
           val result = for {
             i <- values.indices
             j <- i + 1 until values.size
           } yield {
-            ((values(i), values(j)), u)
+            // u < v 는 이미 edgeWithDegree 를 만들 때 처리했으므로
+            // 여기서는 u < v < w 를 맞춰준다.
+            val v_info = values(i)
+            val w_info = values(j)
+            var vw = (v_info._1, w_info._1)
+            if (v_info._2 > w_info._2) {
+              vw = vw.swap
+            }
+            (vw, u)
           }
           result
       }
-      .filter(_.nonEmpty)
       .flatMap(v => v)
-      .groupByKey()
+      .groupByKey() //.foreach(println)
     println("Step 1: End\n")
 
     /* Step 2 : Wedge 와 Edge 를 모두 Input 으로 받아 삼각형 찾기 */
     println("Step 2: Start")
-    val f_edges = edges.map(x => (x, -1)).groupByKey()
-    val f_input = wedges.union(f_edges)
-    //        println("wedgs", wedges.foreach(println))
-    //        println("edges", edges.foreach(println))
-
+    // Wedge 가 정렬된 상태이므로 Edge 도 정렬을 해줘서
+    // 같은 Key 값으로 모일 수 있도록 해준다.
+    val sortedEdges = edgeWithDegree.map {
+      case ((u, ud), (v, vd)) =>
+        ((u, v), -1)
+    }.groupByKey()
+    val f_input = wedges.union(sortedEdges)
     val triangles = f_input
       .groupByKey()
       .flatMap {
@@ -80,31 +105,26 @@ object Task3 {
             } yield IndexedSeq(tri._1._1, tri._1._2, c_vs(i))
             result = joined
           }
-          result.toList
+          result
       }
       .filter(x => x.head != -1)
     println("Step 2: End\n")
 
     println("Step 3: Start\n")
-    // 파일로 저장
-    val fs = FileSystem.get(sc.hadoopConfiguration)
-    val outputPath = new Path(output)
-    if (fs.exists(outputPath)) {
-      fs.delete(outputPath, true)
-    }
+
 
     val counted = triangles
       .flatMap(x => x)
-      .sortBy(x=>x)
+      .sortBy(x => x)
       .countByValue()
-    val counted_save = counted.map(_.productIterator.mkString("\t")).toSeq.sortBy(x=>x.split("\t")(0).toInt)
+    val counted_save = counted.map(_.productIterator.mkString("\t")).toSeq.sortBy(x => x.split("\t")(0).toInt)
     sc.parallelize(counted_save).coalesce(1).saveAsTextFile(output)
 
     println("Step 3: End\n")
 
 
-     // 총 삼각형 개수 출력
-      println("삼각형: " + counted.values.sum / 3 + "개")
+    // 총 삼각형 개수 출력
+    println("삼각형: " + counted.values.sum / 3 + "개")
     sc.stop()
   }
 }
